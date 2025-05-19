8a041dd955ad8a65716f1b4e2be062f80aedec05
Katleho Gxagxa <84070856+KatlehoGxagxa@users.noreply.github.com>
2025-05-19 14:21:08 +0200

    Update problem_set_0.py

--- problem_set_0.py
+++ problem_set_0.py
@@ -1,14 +1,9 @@
+# Author: Katleho Gxagxa
 # I Import the Math module to use the power function and the log2 function.
 import math
 
-# Prompts the user to enter a number x.
-x = int(input("Enter a number 'x': "))
+x = int(input("Enter a number 'x': ")) # Prompts the user to enter a number x.
+y = int(input("Enter a number 'y': ")) # Prompts the user to enter a number y.
 
-# Prompts the user to enter a number y.
-y = int(input("Enter a number 'y': "))
-
-# Prints the number x raised to the power y. Using the Power function.
-print(f'x raised to the power y: {math.pow(x, y)}')
-
-# Prints the log2 of the number x
-print(f'The log (base 2) of x: {math.log2(x)}')
+print(f'x raised to the power y: {math.pow(x, y)}') # Prints the number x raised to the power y. Using the Power function.
+print(f'The log (base 2) of x: {math.log2(x)}') # Prints the log2 of the number x
