a2ed9bea43b9a5e70e8c78950bad42bdb173052c
Katleho Gxagxa <84070856+KatlehoGxagxa@users.noreply.github.com>
2025-05-19 14:02:39 +0200

    The complete problem set 0.

---
+++ problem_set_0.py
@@ -0,0 +1,6 @@
+import math
+
+x = int(input("Enter a number '\x': "))
+y = int(input("Enter a number '\y': "))
+print(f'x raised to the power y: {x.pow(y)}')
+print(f'The log (base 2) of x: {math.log2(x)}')
